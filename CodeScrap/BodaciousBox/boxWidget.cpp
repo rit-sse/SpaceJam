991cd7c01fefdd9956528df9cd502a2c15994ebb
Jesse Jurman <j.r.jurman@gmail.com>
2013-01-19 23:28:36 -0500

    look\! it's a box\!

--- CodeScrap/BodaciousBox/boxWidget.cpp
+++ CodeScrap/BodaciousBox/boxWidget.cpp
@@ -4,41 +4,20 @@
 
 BoxWidget::BoxWidget( QWidget *parent) : QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
 {
-	background = QBrush(Qt::red);
-	hipPen = QPen(Qt::green);
-	currentPen = QPen(Qt::yellow);
-	darkPen = QPen(Qt::black);
-	curFontSize = MAX_FONT;
+	background = QBrush(Qt::blue);
+	hipPen = QPen(Qt::white);
 	elapsed = 0;
-    currentMode = HIP;
     setFixedSize(200, 200);
 }
 
 void BoxWidget::paint(QPainter *painter, QPaintEvent *event)
 {
 	painter->fillRect(event->rect(), background);
-    QString text;
     QPen pen;
     painter->save();
-	switch(currentMode){
-		case HIP:
-            pen = hipPen;
-			text = "So Hip!";
-			break;
-		case CURRENT:
-            pen = currentPen;
-			text = "So Current!";
-			break;
-		case DARK:
-            pen = darkPen;
-			text = "So Dark!";
-			break;
-	}
-    textFont.setPixelSize(MAX_FONT - elapsed);
-    painter->restore();
+	painter->restore();
     painter->setPen(pen);
-	painter->setFont(textFont);
-    painter->drawText(QRect(0, 0, 200, 200), Qt::AlignCenter, text);
+	painter->drawRoundedRect(QRect(50, 50, 100, 100), 0, 0);
 
 }
 
@@ -54,22 +33,8 @@ void BoxWidget::paintEvent(QPaintEvent *event)
 void BoxWidget::animate()
 {
     elapsed = (elapsed + qobject_cast<QTimer*>(sender())->interval()) % 50;
-    if(elapsed == 0){
-        switchMode();
-    }
-	repaint();
+    repaint();
 }
 
 void BoxWidget::switchMode(){
-    switch(currentMode){
-    case HIP:
-        currentMode = CURRENT;
-        break;
-    case CURRENT:
-        currentMode = DARK;
-        break;
-    case DARK:
-        currentMode = HIP;
-        break;
-    }
 }
