af2eb0b3a8538e901f29929992deb1900035c5c8
Lee Avital <leeavital@gmail.com>
2013-03-06 15:14:11 -0500

    Added sample PIL code to main.py

--- CodeScrap/PIL/main.py
+++ CodeScrap/PIL/main.py
@@ -1,1 +1,22 @@
-# something goes here
+# smport Image, ImageDraw
+ 
+import Image, ImageDraw
+ 
+ 
+def pil_image():
+   size = (100, 50)
+   im = Image.new( "RGB", size )
+   draw = ImageDraw.Draw( im )
+   red = ( 255, 0, 0 )
+   text_pos = (10, 10)
+   text = "Hello World!"
+
+
+   draw.text( text_pos, text, fill=red )
+   im.save( "hello.png", "PNG" )
+	 
+	  
+	   
+
+
+pil_image()
