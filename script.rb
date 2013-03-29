f0fe2e56df13ce151a4d476c11b992b861a526d1
Jesse Jurman <j.r.jurman@gmail.com>
2013-03-29 17:56:21 -0400

    new images

--- script.rb
+++ script.rb
@@ -1,3 +1,3 @@
 ("0001".."0096").each do |num|
-  `convert ./monkeySet/#{num}.bmp -resize 608x684 -depth 1 -colors 2 ./bitSet/#{num}.bmp`
+  `convert ./monkeySet2/#{num}.bmp -resize 608x684 -depth 1 -colors 2 ./bitSet/#{num}.bmp`
 end
