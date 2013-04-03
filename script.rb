bde5b6b0492eba4fd213af466a4a856e83320c07
Ethan H. Jurman <ehj2229@rit.edu>
2013-04-03 16:09:20 -0400

    changed the script source for output

--- script.rb
+++ script.rb
@@ -1,3 +1,3 @@
 ("0000".."0095").each do |num|
-  `convert ./monkeySet2/#{num}.bmp -resize 608x684 -depth 1 -colors 2 ./bitSet/#{num}.bmp`
+  `convert ./blenderOutput/#{num}.bmp -resize 608x684 -depth 1 -colors 2 ./bitSet/#{num}.bmp`
 end
