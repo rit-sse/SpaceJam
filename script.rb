7d073ae4c66c7545d8c86e3421e3e361219cbbdb
Ethan H. Jurman <ehj2229@rit.edu>
2013-04-03 22:26:04 -0400

    new script, new blend file, new everything, all good

--- script.rb
+++ script.rb
@@ -1,3 +1,13 @@
-("0000".."0095").each do |num|
-  `convert ./blenderOutput/#{num}.png -resize 608x684 -depth 1 -colors 2 ./bitSet/#{num}.bmp`
+print("do you want to replace images that have already been converted? (Y/n): ")
+input = gets.chomp
+images = File.join("**", "*.png")
+Dir.glob(images).each do |image|
+  outFile = "./bitFolder/#{image.split(".")[0]}.bmp"
+  outFolder = outFile.split("/")[0..-2].join("\\")+"\""
+  if !(Dir.exists?(outFolder[2..-2]))
+    `mkdir #{outFolder[2..-2]}`
+  end
+  if !(input=="n" && File.file?(outFile))
+    `convert ./#{image} -resize 608x684 -depth 1 -colors 2 #{outFile}`
+  end
 end
