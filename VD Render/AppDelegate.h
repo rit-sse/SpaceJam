3ce109d217508530d2d67da737ca054bb4526b97
Michael Timbrook <mpt2360@rit.edu>
2013-04-25 17:37:45 -0400

    Initial Commit

---
+++ VD Render/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VD Render
+//
+//  Created by Michael Timbrook on 4/25/13.
+//  Copyright (c) 2013 MichaelTimbrook. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
