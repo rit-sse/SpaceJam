3467b0bf23bc430f3849a61d48a8684528b5a186
Michael Timbrook <mpt2360@rit.edu>
2013-04-29 18:12:44 -0400

    OpenGL Renderer, with distortion and non functional PNG exporter

---
+++ OpenGLRenderer/VD Render/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  VD Render
+//
+//  Created by Michael Timbrook on 4/25/13.
+//  Copyright (c) 2013 MichaelTimbrook. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
